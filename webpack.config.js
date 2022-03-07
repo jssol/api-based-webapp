6abaf05764bb550b24a6637f7061de731d682a3b
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-07 23:51:00 +0200

    Install postcss

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
     ],
   },
