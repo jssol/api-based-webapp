4801bb75537ce0f79f10e8dec8d16a94b8bea8d3
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-03-08 08:28:00 +0100

    Updates

--- main.js
+++ main.js
@@ -6,15 +6,24 @@ var __webpack_exports__ = {};
 const getData = async (movie = 'the flash') => {
   const key = 'e7db26be';
 
-  const data = await fetch(`http://www.omdbapi.com/?s=${movie}&apikey=${key}`);
+  const data = await fetch(`https://www.omdbapi.com/?s=${movie}&apikey=${key}`);
   const moviesInfo = await data.json();
   return moviesInfo.Search;
 };
 
+// const movies = [];
+// getData().then((data) => {
+//   data.forEach((movie) => {
+//     movies.push(movie);
+//   });
+// });
+
+// console.log(movies);
+
 const getMovieData = async (movie) => {
   const key = 'e7db26be';
 
-  const data = await fetch(`http://www.omdbapi.com/?t=${movie}&plot=full&apikey=${key}`);
+  const data = await fetch(`https://www.omdbapi.com/?t=${movie}&plot=full&apikey=${key}`);
   const moviesInfo = await data.json();
   console.log(moviesInfo.Plot);
   return moviesInfo;
