71f383f891298176e4dd75eee21debad4156a424
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-08 12:05:29 +0200

    Fix comments popup

--- src/index.js
+++ src/index.js
@@ -4,39 +4,46 @@ import { getData, getMovieData } from './modules/api.js';
 const movieList = document.querySelector('.movie-list');
 const movieDetails = document.querySelector('.movie-details');
 
-getData()
-  .then((res) => {
-    res.forEach((movie) => {
-      movieList.innerHTML += `<article class="movie">
+const displayMovies = () => {
+  getData()
+    .then((res) => {
+      res.forEach((movie) => {
+        movieList.innerHTML += `<article class="movie">
                                 <img class="movie-poster" src="${movie.Poster}"/>
                                 <p class="movie-title">${movie.Title}</p>
                                 <ul class="type-year">
                                     <li class="movie-type">${movie.Type}</li>
                                     <li class="movie-year">${movie.Year}</li>
                                 </ul>
+                                <button class="comment">Comment</button>
                             </article>`;
-    });
-  })
-  .then(() => {
-    document.querySelectorAll('.movie-poster').forEach((btn) => {
-      btn.addEventListener('click', () => {
-        getMovieData(btn.nextElementSibling.innerHTML).then((data) => {
-          movieList.style.display = 'none';
-          movieDetails.innerHTML = `<article class="m">
-                                <img class="m-poster" src="${data.Poster}"/>
-                                <div class="m-title-plot">
-                                    <p class="m-title">${data.Title}</p>
-                                    <p class="m-plot">${data.Plot}</p>
-                                </div>
-                                <ul class="type-year">
-                                    <li class="movie-type">${data.Type}</li>
-                                    <li class="movie-year">${data.Year}</li>
-                                </ul>
-                            </article>`;
-        });
       });
     });
-  })
-  .catch((e) => {
-    throw new Error(e);
-  });
+};
+
+const showComment = (btn) => {
+  const movie = btn.previousElementSibling.previousElementSibling.innerHTML;
+  getMovieData(movie)
+    .then((data) => {
+      movieList.style.display = 'none';
+      movieDetails.innerHTML = `<article class="m">
+                          <img class="m-poster" src="${data.Poster}"/>
+                          <div class="m-title-plot">
+                              <p class="m-title">${data.Title}</p>
+                              <p class="m-plot">${data.Plot}</p>
+                          </div>
+                          <ul class="type-year">
+                              <li class="movie-type">${data.Type}</li>
+                              <li class="movie-year">${data.Year}</li>
+                          </ul>
+                      </article>`;
+    });
+};
+
+document.addEventListener('DOMContentLoaded', displayMovies);
+
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.classList.contains('comment')) {
+    showComment(e.target);
+  }
+});
--- src/modules/api.js
+++ src/modules/api.js
@@ -6,21 +6,11 @@ const getData = async (movie = 'the flash') => {
   return moviesInfo.Search;
 };
 
-// const movies = [];
-// getData().then((data) => {
-//   data.forEach((movie) => {
-//     movies.push(movie);
-//   });
-// });
-
-// console.log(movies);
-
 const getMovieData = async (movie) => {
   const key = 'e7db26be';
 
   const data = await fetch(`https://www.omdbapi.com/?t=${movie}&plot=full&apikey=${key}`);
   const moviesInfo = await data.json();
-  console.log(moviesInfo.Plot);
   return moviesInfo;
 };
 export { getData, getMovieData };
