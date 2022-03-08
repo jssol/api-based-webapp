5c9bfc6c33c8ba87f05218f128a5b3b6e6f2586f
Jonathan Sivahera <jsivahera@icloud.com>
2022-03-08 22:10:39 +0200

    Style the comments popup

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ const page = document.documentElement;
 const displayMovies = () => {
   getData()
     .then((res) => {
-      res.forEach((movie) => {
-        movieList.innerHTML += `<article class="movie">
+      res.forEach((movie, index) => {
+        const identity = (movie) => {
+          const id = movie.Title.toLowerCase().replace(/\s/g, '-');
+          return id;
+        };
+        movieList.innerHTML += `<article id="${identity(movie)}" class="movie">
                                 <img class="movie-poster" src="${movie.Poster}"/>
                                 <div class="l-c-buttons">
                                     <i class="like-btn">&#x2764;</i>
-                                    <button class="comment-btn">Comment</button>
+                                    <button class="comment-btn">Comments</button>
                                 </div>
                                 <p class="movie-title">${movie.Title}</p>
                                 <ul class="type-year">
@@ -31,17 +35,37 @@ const showComment = (btn) => {
   page.classList.add('comment-open');
   getMovieData(movie)
     .then((data) => {
-      movieDetails.innerHTML = `<button class="pop-close-btn btn"><span class="pop-close"></span></button>
-      <article class="m">
-        <img class="m-poster" src="${data.Poster}"/>
-        <div class="m-title-plot">
-            <p class="m-title">${data.Title}</p>
+      movieDetails.innerHTML = `
+      <article class="movie-popup">
+        <button class="pop-close-btn btn"><span class="pop-close"></span></button>
+        <section class="main-popup-content">
+          <section class="movie-img">
+            <img class="m-poster" src="${data.Poster}"/>
+          </section>
+          <section class="m-title-plot">
+            <h2 class="m-title">${data.Title}</h2>
+            <ul class="type-year">
+              <li class="movie-meta-info">${data.Type}</li>
+              <li class="movie-meta-info">${data.Year}</li>
+            </ul>
             <p class="m-plot">${data.Plot}</p>
-        </div>
-        <ul class="type-year">
-            <li class="movie-type">${data.Type}</li>
-            <li class="movie-year">${data.Year}</li>
-        </ul>
+            <section class="movie-comments">
+              <h3 class="comments-subtitle">Comments</h3>
+              <ul class="comments-list list"></ul>
+              <h3 class="comments-subtitle">Add a comment</h3>
+              <form action="#">
+                <ul class="input-list list">
+                  <li class="input-list-item">
+                    <textarea name="comment" id="comment" class="comment-input" placeholder="Give us your thoughts ..."></textarea>
+                  </li>
+                  <li class="input-list-item">
+                    <button type="submit" class="comment-submit btn">Comment</button>
+                  </li>
+                </ul>
+              </form>
+            </section>
+          </section>
+        </section>
       </article>`;
     });
 };
@@ -53,7 +77,7 @@ document.addEventListener('click', (e) => {
     showComment(e.target);
   }
 
-  if (e.target && e.target.classList.contains('pop-close-btn')) {
+  if (e.target && (e.target.classList.contains('pop-close-btn') || e.target.classList.contains('pop-close'))) {
     page.classList.remove('comment-open');
   }
 });
--- src/modules/involvement.api.js
+++ src/modules/involvement.api.js
@@ -1,1 +1,5 @@
 const addIdentifier = 'YUqI88f5a8VwBEjald5b';
+
+const getComments = async (movie) => {
+
+};
